42d71acda82486e5cc0ef766dda849f492091ed2
Kenny <lli15@stuy.edu>
2017-11-11 20:39:35 -0500

    v2

--- v1/Protagonist.java
+++ v1/Protagonist.java
@@ -8,7 +8,7 @@ public class Protagonist{
     private String name;
     private int health;
     private int strength;
-    public int defense;
+    private int defense;
     private double attack;
     
     //constructor
