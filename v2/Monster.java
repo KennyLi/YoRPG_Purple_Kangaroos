d35a34d1925d711ee41eb80c6938ed04196b8769
Kenny <lli15@stuy.edu>
2017-11-11 20:11:57 -0500

    v2

---
+++ v2/Monster.java
@@ -0,0 +1,43 @@
+//Purple Kangaroos- Clarence Cheng, Kenny Li, Simon Tsui
+//APCS1 pd 1
+//HW#28: Ye Olde Role Playing Game
+//2017-11-08
+
+public class Monster{
+    //instance variables
+    public int health;
+    public int strength;
+    public int defense;
+    public double attack;
+    
+    //constructor
+    public Monster(){
+        health = 150;
+        strength = (int)(Math.random() * 45) + 20;
+        defense = 20;
+        attack = 1.0;
+    }
+        
+    public boolean isAlive(){
+	//the monster is defined as "alive" if it has positive value for health
+        return health > 0;
+    }
+        
+    public int getDefense(){
+        return defense;   //accesses defense val
+    }
+    
+    public void lowerHP(int damage){   
+        health -= damage;    //no return value necessary, lowers health by value damage
+      
+    }
+		
+    public int attack(Protagonist protagonist){
+        int damage = (int)(strength * attack) - protagonist.defense; //calculates dmg
+	if (damage < 0) {
+	    damage = 0;
+	}
+	protagonist.lowerHP(damage);
+        return damage;
+    }
+}
---
+++ v2/Protagonist.java
@@ -0,0 +1,59 @@
+//Purple Kangaroos- Clarence Cheng, Kenny Li, Simon Tsui
+//APCS1 pd 1
+//HW#28: Ye Olde Role Playing Game
+//2017-11-08
+
+public class Protagonist{
+    //instance variables
+    private String name;
+    private int health;
+    private int strength;
+    public int defense;
+    private double attack;
+    
+    //constructor
+    public Protagonist(String newName){
+        name = newName;
+        health = 125;
+        strength = 100;
+        defense = 40;
+        attack = 0.5;
+    }
+		
+    public boolean isAlive(){
+        //the character is defined as "alive" if it has positive value for health
+        return health > 0;
+    }
+		
+    public int getDefense(){
+        return defense;   //returns defense val
+    }
+		
+    public String getName(){
+        return name;      //returns name val
+    }
+		
+    public void lowerHP(int damage){   
+        health -= damage;    //no return value necessary, lowers health by value damage
+    }
+		
+    public int attack(Monster monster){
+        int damage = (int)(strength * attack) - monster.defense; //calculates dmg
+	if (damage < 0) {
+	    damage = 0;
+	}
+	
+	monster.lowerHP(damage);
+        return damage;
+    }
+    
+    public void specialize(){
+        defense -= 2; //performs special attack, lowers defense, increases atttack rating
+        attack += 0.5;
+    }
+		
+    public void normalize(){
+        defense += 2;     //reverses effects of specialize
+        attack -= 0.5;
+    }
+}
